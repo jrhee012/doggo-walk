70130ca70ae5d7912fad594e3ba92f181eba851f
Jaehyuk Rhee <jaehyuk.rhee@gmail.com>
2019-04-09 10:41:25 -0400

    Initial Commit

---
+++ doggo-walk/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  doggo-walk
+//
+//  Created by Jaehyuk Rhee on 4/9/19.
+//  Copyright © 2019 Jaehyuk Rhee. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ doggo-walk/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  doggo-walk
+//
+//  Created by Jaehyuk Rhee on 4/9/19.
+//  Copyright © 2019 Jaehyuk Rhee. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
